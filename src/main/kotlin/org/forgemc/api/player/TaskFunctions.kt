ace36721c139f6c4234cc8c45811264df268fcc0
Graviton <44570024+Graviton1647@users.noreply.github.com>
2020-05-30 18:58:26 +0100

    Task Functions

---
+++ src/main/kotlin/org/forgemc/api/player/TaskFunctions.kt
@@ -0,0 +1,47 @@
+package org.forgemc.api.player
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.Plugin
+
+import org.bukkit.scheduler.BukkitRunnable
+
+
+/**
+ * Delay an action by a number of seconds
+ * @param plugin plugin instance
+ * @param delay numbers of seconds to wait
+ */
+fun delayBy(plugin : Plugin,delay : Int, task : () -> Unit ) {
+    object : BukkitRunnable() {
+        override fun run() {
+            task.invoke()
+        }
+    }.runTaskLater(plugin, 20L * delay)
+}
+
+/**
+ * Run a action
+ * @param plugin plugin instance
+ */
+fun runTask(plugin : Plugin, task : () -> Unit ) {
+    object : BukkitRunnable() {
+        override fun run() {
+            task.invoke()
+        }
+    }.runTask(plugin)
+}
+
+/**
+ * Delay an action by a number of seconds adn wait by a number of seconds
+ * @param plugin plugin instance
+ * @param wait numbers of seconds to start
+ * @param delay numbers of seconds to wait
+ */
+
+fun delayByWait(plugin : Plugin, wait : Int, delay : Int, task : () -> Unit ) {
+    object : BukkitRunnable() {
+        override fun run() {
+            task.invoke()
+        }
+    }.runTaskTimer(plugin,20L * wait,20L * delay)
+}
