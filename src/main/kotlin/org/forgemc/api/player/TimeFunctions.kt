c19b37555ba8f9078cd21e7d1e7427789cc5b0b9
Graviton <44570024+Graviton1647@users.noreply.github.com>
2020-05-30 20:16:36 +0100

    Update TimeFunctions.kt

--- src/main/kotlin/org/forgemc/api/player/TimeFunctions.kt
+++ src/main/kotlin/org/forgemc/api/player/TimeFunctions.kt
@@ -2,6 +2,5 @@ package org.forgemc.api.player
 
 import java.util.concurrent.TimeUnit
 
-fun toSeconds(mins : Int) {
-    TimeUnit.MINUTES.toSeconds(mins.toLong())
-}
+fun toSeconds(mins : Int) : Long = TimeUnit.MINUTES.toSeconds(mins.toLong())
+
