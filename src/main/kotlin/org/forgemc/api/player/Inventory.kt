915831d45f1975f1c865caeaa76003ce815bb6d2
Graviton <44570024+Graviton1647@users.noreply.github.com>
2020-05-31 18:10:31 +0100

    Create Inventory.kt

---
+++ src/main/kotlin/org/forgemc/api/player/Inventory.kt
@@ -0,0 +1,23 @@
+package org.forgemc.api.player
+
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.SkullMeta
+import java.util.*
+import java.util.stream.Collectors
+
+fun ItemStack.getPlayerHead(owner: String) : ItemStack {
+    val newVersion = Arrays.stream(Material.values()).map { it.name }.collect(Collectors.toList()).contains("PLAYER_HEAD")
+    val type = Material.matchMaterial(
+        if(newVersion) "PLAYER_HEAD" else "SKULL_ITEM"
+    )
+    val item = ItemStack(type,1)
+    if(!newVersion) {
+        item.durability = 3 as Short
+    }
+    val meta = item.itemMeta as SkullMeta
+    meta.owningPlayer = Bukkit.getPlayer(UUID.fromString(owner))
+    item.itemMeta = meta
+    return item
+}
