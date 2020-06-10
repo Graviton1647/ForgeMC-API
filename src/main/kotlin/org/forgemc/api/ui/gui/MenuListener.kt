5375d16a5db2e257609aeb91ab847afc65a94da9
Graviton <44570024+Graviton1647@users.noreply.github.com>
2020-06-10 18:40:27 +0100

    Update MenuListener.kt

--- src/main/kotlin/org/forgemc/api/ui/gui/MenuListener.kt
+++ src/main/kotlin/org/forgemc/api/ui/gui/MenuListener.kt
@@ -1,15 +1,12 @@
 package org.forgemc.api.ui.gui
 
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.inventory.InventoryCloseEvent
 import org.bukkit.event.inventory.InventoryOpenEvent
 import org.bukkit.inventory.InventoryHolder
-import org.forgemc.api.events.MinecraftEvent
 
-@MinecraftEvent("menu")
 class MenuListener : Listener {
 
     @EventHandler
