01c221b4c683eb8b765294c3cd660d873f0a7523
Graviton <44570024+Graviton1647@users.noreply.github.com>
2020-06-13 05:52:13 +0100

    Update ActionBar

--- src/main/kotlin/org/forgemc/api/actionbar/ServerPackage.kt
+++ src/main/kotlin/org/forgemc/api/actionbar/ServerPackage.kt
@@ -6,6 +6,10 @@ internal enum class ServerPackage(private val path: String) {
     MINECRAFT("net.minecraft.server.${Bukkit.getServer().javaClass.getPackage().name.substring(23)}"),
     CRAFTBUKKIT("org.bukkit.craftbukkit.${Bukkit.getServer().javaClass.getPackage().name.substring(23)}");
 
+    override fun toString(): String {
+        return path
+    }
+
     @Throws(ClassNotFoundException::class)
     fun getClass(className: String): Class<*> {
         return Class.forName("$this.$className")
